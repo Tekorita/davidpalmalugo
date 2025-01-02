d1c7e19919042e7eae7fcd3bfd24388b70de53cc
Tekorita <davidpalmalugo@gmail.com>
2025-01-02 00:52:24 -0400

    new changes in config of static files

--- davidpalmalugo/settings.py
+++ davidpalmalugo/settings.py
@@ -133,8 +133,11 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Configuración para servir archivos estáticos en producción
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (

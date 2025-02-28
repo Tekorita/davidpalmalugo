0f3d622c04b9cc36bffcb4edbd8d05308b21eeee
Tekorita <davidpalmalugo@gmail.com>
2025-02-28 05:12:34 -0400

    fix allowed host of settings

--- davidpalmalugo/settings.py
+++ davidpalmalugo/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = 's-26@br@k3midwh714n-@*uhy2-16ly)^q-w8)6o$=j&rr6_^-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# ALLOWED_HOSTS = [] #para uso local
-ALLOWED_HOSTS = ['*'] #para uso local
-# ALLOWED_HOSTS = ['davidpalmalugo.com'] #para uso en heroku
+ALLOWED_HOSTS = ['davidpalmalugo.com', 'www.davidpalmalugo.com'] #para uso en heroku
 
 
 # Application definition

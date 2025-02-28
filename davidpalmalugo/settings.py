5ee762ea99c82596432685499ae706949b5ed0d5
Tekorita <davidpalmalugo@gmail.com>
2025-02-28 18:48:52 -0400

    Primer deploy limpio a Heroku

--- davidpalmalugo/settings.py
+++ davidpalmalugo/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 's-26@br@k3midwh714n-@*uhy2-16ly)^q-w8)6o$=j&rr6_^-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['davidpalmalugo.com', 'www.davidpalmalugo.com', 'davidpalmalugo.herokuapp.com']
-
+ALLOWED_HOSTS = ['*'] #para uso en heroku
 
 
 # Application definition
